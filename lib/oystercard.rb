61f69603d6cc53440eb63618c77a6ff23bc0bf8f
edpackard <edpackard81@gmail.com>
2021-08-02 16:35:21 +0100

    Chapter 6: added unit tests, passing code, and refactoring

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
   
   INITIAL_BALANCE = 0
+  TOP_UP_LIMIT = 90
   attr_reader :balance
 
   def initialize 
@@ -8,6 +9,14 @@ class Oystercard
   end
 
   def top_up(amount)
+    raise "Maximum top up (£#{TOP_UP_LIMIT}) exceeded" if exceed_top_up?(amount,balance)
     @balance += amount
   end
+
+  private
+
+  def exceed_top_up?(amount, balance)
+    (balance += amount) > TOP_UP_LIMIT
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,13 +2,26 @@ require 'oystercard'
 
 describe Oystercard do
 
-	it "gives an initial balance of 0" do
-  	expect(subject.balance).to eq 0
-	end
+  describe '#balance' do
 
-	it "tops up the balance on the card" do
-    expect { subject.top_up(10)}.to change { subject.balance }.from(0).to(10)
+	  it "gives an initial balance of 0" do
+    	expect(subject.balance).to eq 0
+	  end
 
-	end
+  end
+
+  describe '#top_up' do
+
+  	it "tops up the balance on the card" do
+      expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+	  end
+
+    it "raises an error if user tries to top up above £90" do
+      top_up_limit = Oystercard::TOP_UP_LIMIT
+      error_message = "Maximum top up (£#{top_up_limit}) exceeded"
+      expect { subject.top_up(top_up_limit+1) }.to raise_error error_message
+    end
+
+  end
 
 end
